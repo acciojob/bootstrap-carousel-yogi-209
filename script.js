0122b2bcad00f724fec9a566c89530388eef3ce3
Boyanapalli Yogendra sai <boyanapalliyogendrasai@gmail.com>
2026-09-04 21:59:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-//your code here
 $(document).ready(function () {
+
+    // Initialize carousel
     $('#myCarousel').carousel();
+
+    // Next slide
+    $('.right').click(function () {
+        $('#myCarousel').carousel('next');
+    });
+
+    // Previous slide
+    $('.left').click(function () {
+        $('#myCarousel').carousel('prev');
+    });
+
 });
